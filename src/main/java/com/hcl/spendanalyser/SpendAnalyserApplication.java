12b83d94f7cfa6185998f7ad2be6f912ecf1437f
srao79 <srao79@gmail.com>
2019-05-21 07:07:09 +0000

    initial draft

---
+++ src/main/java/com/hcl/spendanalyser/SpendAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.spendanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendAnalyserApplication.class, args);
+	}
+
+}
